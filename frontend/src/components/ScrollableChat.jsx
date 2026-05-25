d67a756d04fa36732a7514c5bb5739b8cb3431a1
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-05-26 02:40:25 +0500

    feat: Enhance ScrollableChat component with user avatars and message styling for improved chat display

--- frontend/src/components/ScrollableChat.jsx
+++ frontend/src/components/ScrollableChat.jsx
@@ -1,9 +1,34 @@
 import ScrollableFeed from 'react-scrollable-feed'
+import Tooltip from '@mui/material/Tooltip';
+import ChatContext from '../context/ChatContext'
+import { useContext } from 'react'
+import { isSameSender, isLastMessage, isSameSenderMargin, isSameUser } from '../config/chatLogics'
+import Avatar from '@mui/material/Avatar';
+
+
+
 const ScrollableChat = ({ messages }) => {
+    const { user } = useContext(ChatContext)
+
     return (
         <ScrollableFeed>
             {messages && messages.map((m, i) => (
-                <div style={{ display: 'flex' }} key={m._id}></div>
+                <div style={{ display: 'flex' }} key={m._id}>
+                    {isSameSender(messages, m, i, user._id) || isLastMessage(messages, m, i, user._id) && <Tooltip title={m.sender.name} >
+                        <Avatar sx={{ marginTop: "7px", marginRight: 1 }} sizes='sm' alt={m.sender.name} src={m.sender.pic} />
+                    </Tooltip>}
+
+                    <span style={{
+                        backgroundColor: `${m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+                            }`,
+                        borderRadius: '20px',
+                        padding: "5px 15px",
+                        maxWidth: '75%',
+                        marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                        marginTop: isSameUser(messages, m, i) ? 3 : 10
+
+                    }}>{m.content}</span>
+                </div>
             ))}
         </ScrollableFeed>
     )
