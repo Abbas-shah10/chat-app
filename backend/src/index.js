77cc1899ea5cce51e0eb93a54954776788a2108c
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-05-30 08:37:24 +0500

    fix: update route handling for static file serving in production to correct path matching

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,8 +30,8 @@ app.use("/api/v1/messages", messageRoutes);
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
-  app.get("(.*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+  app.get("*any", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", 'frontend', 'dist', 'index.html'));
   });
 
 } else {
